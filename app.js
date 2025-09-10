1232b2d66874103669fca88aaac91c66eedded69
ericson nascimento <ericson@grupocyber.com.br>
2025-09-10 13:58:34 -0300

    feat - finaliza a função calcularMedia(), disponibilizada no switch principal

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ const prompt = require('prompt-sync') ([singint=true]);
 let menu;
 
 //função que chama o menu principal, reduz a repetição de código 
-menuInicial()
+menuInicial();
 
 //criando o loop infinito através do "do while"
 do {
     //prompt principal relacionado ao menu    
     menu = parseInt(prompt('Para reimprimir o MENU(6) ou SAIR(7) | Escolha um código do MENU: '));
-    console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+    console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
 
-    //utilizando o "switch" para gerenciar o MENU, objetivo é que cada CASE chame uma função
+    //utilizando o "switch" para gerenciar o MENU, objetivo é que cada CASE chame uma função entre as oções de 1 à 7
     switch (menu) {
         case 1:
             classificarNadador();
@@ -50,6 +50,11 @@ do {
         case 6:
             menuInicial();
             break;
+
+        case 7:
+            console.log('=-=-=-=-Obrigado por utilizar nosso sistema. Volte Sempre!=-=-=-=-=');
+            console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+            break;
         
         default:
             console.log('Error - Digita um código válido');
@@ -59,45 +64,83 @@ do {
 //condicional do "do while" para sair do loop infinito
 } while (menu != 7);
 
-//função de classificação do nadador
-function classificarNadador() {   
-    idade = parseInt(prompt('Digite a idade do nadador: '))
+//função de classificação de nadador por idade
+function classificarNadador() {
+    //variável de coleta da idade   
+    let idade = parseInt(prompt('Digite a idade do nadador: '));
     
     //primeiro if de verificação para menores de 4 anos ou números negativos
     if (idade <= 4) {
         console.log('Nadador não tem idade suficiente!');
-        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     } 
     else if (idade >= 5 && idade <= 7) {
         console.log('Nadador pertence ao INFANTIL A');
-        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     } 
     else if (idade >= 8 && idade <= 10) {
         console.log('Nadador pertence ao INFANTIL B');
-        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     } 
     else if (idade >= 11 && idade <= 13) {
         console.log('Nadador pertence ao JUVENIL A');
-        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     } 
     else if (idade >= 14 && idade <= 17) {
         console.log('Nadador pertence ao JUVENIL B');
-        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     } 
     else if (idade >= 18) {
         console.log('Nadador é MAIOR de 18 anos.');
-        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     } 
     else {
         //tratamento de entrada inválida como strings
         console.log('Error - Digite um valor válido!');
-        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=')
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     }
 }
 
+//função para cálculo da média com menu de escolha de duas opções
 function calcularMedia() {
-    let nun = parseInt(prompt('Digite um número: '))
-    console.log('Média')
+    //menu principal da função com legenda de opções de escolha
+    console.log('-= FUNÇÃO MÉDIA DE TRÊS NOTAS -=-');
+    console.log('| Código | Cargo                |');
+    console.log('|--------|----------------------|');
+    console.log('| 1      | Aritimética          |');
+    console.log('| 2      | Ponderada (3,3,4)    |');
+    console.log('| 3      | Hamônica             |');
+    console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+
+    //variável relacionada ao menu
+    let menu = parseInt(prompt('Escolha um código do MENU:  '));
+
+    //variáveis de coleta das notas
+    let nota1 = parseInt(prompt('Digite a primeira nota: '));
+    let nota2 = parseInt(prompt('Digite a segunda nota: '));
+    let nota3 = parseInt(prompt('Digite a terceira nota: '));
+
+    //switch que relaciona a variável menu com as opçoes escolhidas entre 1, 2 e 3
+    switch (menu) {
+        case 1:
+            let mediaAritmetica = (nota1 + nota2 + nota3) / 3;
+            console.log(`A média Aritimética das três notas é: ${mediaAritmetica.toFixed(2)}`);
+            console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+            break;
+
+        case 2:
+            let mediaPonderada = (nota1 * 3 + nota2 * 3 + nota3 * 4) / (3 + 3 + 4);
+            console.log(`A média Ponderada(pesos 3,3,4) das três notas é: ${mediaPonderada.toFixed(2)}`);
+            console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+            break;
+        
+        case 3:
+            let mediaHamonica = 3 / (1 / nota1 + 1 / nota2 + 1 / nota3);
+            console.log(`A média Harmônica das três notas é: ${mediaHamonica.toFixed(2)}`);
+            console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+            break;
+    }
+    
 }
 
 function calcularCredito() {
@@ -116,15 +159,17 @@ function calcularAumento() {
 }
 
 function menuInicial() {
-    console.log('=-=-=-=-=-=-=-=-=-=-=-=-MENU DE PARA FUNÇÕES ÚTEIS=-=-=-=-=-=-=-=-=-=-=-=-');
+    console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-');
+    console.log('|                                                                        |');
+    console.log('=-=-=-=-=-=-=-=-=-=-=-=- SISTEMA DE FUNÇÕES ÚTEIS =-=-=-=-=-=-=-=-=-=-=-=-');
     console.log('| Código | Função                    | Explicação                        |');
     console.log('|--------|---------------------------|-----------------------------------|');
-    console.log('| 1      | Classificar Nadador       | Turma x Idade                     |');
-    console.log('| 2      | Calcular Média            | Média entre 3 notas               |');
-    console.log('| 3      | Calcular Cérdito          | Retorna valor médio e crédito     |');
-    console.log('| 4      | Calcular Débito Total     | Quant. de produto com valor total |');
-    console.log('| 5      | Calcular Aumento Salarial | Acrércimo por percentual          |');
-    console.log('| 6      | Menu                      | Impressão do Menu                 |');
-    console.log('| 7      | Sair                      | Sair do sistema                   |');
+    console.log('|    1   | Classificar Nadador       | Turma x Idade                     |');
+    console.log('|    2   | Calcular Média            | Média entre 3 notas               |');
+    console.log('|    3   | Calcular Cérdito          | Retorna valor médio e crédito     |');
+    console.log('|    4   | Calcular Débito Total     | Quant. de produto com valor total |');
+    console.log('|    5   | Calcular Aumento Salarial | Acrércimo por percentual          |');
+    console.log('|    6   | Menu                      | Impressão do Menu                 |');
+    console.log('|    7   | Sair                      | Sair do sistema                   |');
     console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-');
 }
